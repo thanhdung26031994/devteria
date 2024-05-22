8b7f5a42c0f58c27a8adab3201da738cb6e1fe6c
thanhdung26031994 <thanhdung26031994@gmail.com>
2024-05-22 12:00:23 +0700

    validation

--- src/main/java/com/huynhdung/controller/UserController.java
+++ src/main/java/com/huynhdung/controller/UserController.java
@@ -4,6 +4,7 @@ import com.huynhdung.dto.request.UserCreationRequest;
 import com.huynhdung.dto.request.UserUpdateRequest;
 import com.huynhdung.entity.User;
 import com.huynhdung.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,7 +17,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    User createUser(@RequestBody UserCreationRequest request){
+    User createUser(@RequestBody @Valid UserCreationRequest request){
         return userService.createRequest(request);
     }
 

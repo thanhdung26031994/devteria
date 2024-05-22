720c7226d55868dd11d79ec5ebb1d193bf9473fb
thanhdung26031994 <thanhdung26031994@gmail.com>
2024-05-22 11:35:01 +0700

    exception

--- src/main/java/com/huynhdung/exception/GlobalException.java
+++ src/main/java/com/huynhdung/exception/GlobalException.java
@@ -1,8 +1,14 @@
 package com.huynhdung.exception;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalException {
 
+    @ExceptionHandler(value = RuntimeException.class)
+    ResponseEntity<String> handlingRuntimeException(RuntimeException exception){
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
 }
